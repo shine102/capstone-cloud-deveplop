bde7b679007357297732bb3682fbd4facbb9ce09
shine102 <nguyenngotunglam2@gmail.com>
2022-12-26 15:11:29 +0700

    refactor

--- api-photo/src/controllers/v0/model.index.ts
+++
@@ -1,5 +0,0 @@
-
-import {Photo} from './models/Photo';
-
-
-export const V0_PHOTO_MODELS = [Photo];
--- api-user/src/controllers/v0/model.index.ts
+++
@@ -1,5 +0,0 @@
-
-import {User} from './models/User';
-
-
-export const V0_USER_MODELS = [User];
