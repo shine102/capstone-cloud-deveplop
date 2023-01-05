aaa164d18a9ed70de4330d6b4d67f82934919be6
shine102 <nguyenngotunglam2@gmail.com>
2023-01-05 15:53:31 +0700

    fix port

--- udagram-api-user/src/app.ts
+++ udagram-api-user/src/app.ts
@@ -3,8 +3,8 @@ import connectDb from "./connectdb";
 
 const app = startServer();
 
-app.listen(8100, async () => {
-    console.log("server started on port 8100");
+app.listen(8080, async () => {
+    console.log("server started on port 8080");
     await connectDb();
     }
 );
